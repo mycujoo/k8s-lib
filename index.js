46e6c4d1b8696e4a705bce602d078d85e0c7176b
Stephen Hoogendijk <stephen@tca0.nl>
2017-01-27 17:23:17 +0100

    Fixes according to comments from PR

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cloudflare = require('./lib/cloudflare')
 const k8s = require('./lib/k8s')
 const secrets = require('./lib/secrets')
 const util = require('./lib/util')
-const vault = require('./lib/vault'
+const vault = require('./lib/vault')
 
 module.exports = {
     aws,
