f7c12ad856dcc0a16cb79cacb5ea1fe7e1d23846
Stephen Hoogendijk <stephen@tca0.nl>
2017-01-27 15:52:55 +0100

    Initial version 1.0.0

---
+++ index.js
@@ -0,0 +1,15 @@
+const aws = require('./lib/aws')
+const cloudflare = require('./lib/cloudflare')
+const k8s = require('./lib/k8s')
+const secrets = require('./lib/secrets')
+const util = require('./lib/util')
+const vault = require('./lib/vault'
+
+module.exports = {
+    aws,
+    cloudflare,
+    k8s,
+    secrets,
+    util,
+    vault
+}
